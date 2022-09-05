81bde1b618c62e24af4478c15bb0c17ecc3e5fdf
Wesley <wesleybortolosocco@gmail.com>
2022-09-05 00:07:03 -0300

    implementing loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+export default function Loader() {
+  return (
+    <div className='spinner-container'>
+      <div className='loading-spinner'></div>
+    </div>
+  )
+}
